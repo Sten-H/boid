ad8baf03014544b4d7db34474592c30ded11c7ab
fruntimmer <s@stenh.com>
2014-09-26 00:08:43 +0200

    added grid data structure. Prevents n*n time of updating flock velocity. Need higher divisions of grid for even more efficiency. Very rough atm

--- boid.py
+++ boid.py
@@ -3,9 +3,11 @@ __author__ = 'Laxl'
 import random
 from euclid import Vector2
 from math import sqrt
+from math import pow
 
-def length(v1, v2):
-    return sqrt(((v1.x - v2.x)**2) + ((v1.y - v2.y)**2))
+
+    #return (v1-v2).magnitude()
+
 
 boid_colours = [(52, 117, 214), (215, 68, 52), (52, 216, 69), (200, 217, 52)]
 
@@ -21,29 +23,43 @@ class Boid():
         self.colour = random.choice(boid_colours)
         self.look_direction = Vector2(0, 0)
 
+    def __str__(self):
+        return str(self.pos)
+    def __repr__(self):
+        return self.__str__()
+
+    def length(self, v1, v2):
+        x1, x2, y1, y2 = v1.x, v2.x, v1.y, v2.y
+        return sqrt(pow((x1 - x2),2) + pow((y1 - y2), 2))
+
+    def examine_boid(self, b, v):
+        if b == self:
+            return
+        dist = self.length(self.pos, b.pos)
+        if dist < self.proximity_value:
+            v[2] -= (b.pos - self.pos)
+        if dist < self.sight_range:
+            v[0] += b.pos
+            v[1] += b.velocity
+            v[3] += 1
+
     def examine_flock(self, flock):
         #Returns the average perceived center, perceived direction and separation of Boids within sight_range
-        v_sep, p_center, avg_dir = Vector2(0, 0), Vector2(0, 0), Vector2(0, 0)
+        #v_sep, p_center, avg_dir = Vector2(0, 0), Vector2(0, 0), Vector2(0, 0)
         # loop values
-        neighbours = 0
-        tot_pos, tot_dir = Vector2(0, 0), Vector2(0, 0)
-        for b in flock:
-            if b == self:
-                continue
-            dist = length(self.pos, b.pos)
-            if dist < self.sight_range:
-                if dist < self.proximity_value:  # This presumes sight range is never lower than prox value
-                    v_sep -= (b.pos - self.pos)
-                tot_pos += b.pos
-                tot_dir += b.velocity
-                neighbours += 1
+        vectors = [Vector2(0,0), Vector2(0, 0), Vector2(0, 0), 0]
+        [self.examine_boid(b, vectors) for b in flock]
+        #map(self.examine_boid, flock)
+        p_center, avg_dir, v_sep = vectors[0], vectors[1], vectors[2]
+        neighbours = vectors[3]
         if neighbours > 0:
-            p_center = tot_pos / neighbours
-            avg_dir = tot_dir / neighbours
+            p_center = p_center / neighbours
+            avg_dir = avg_dir / neighbours
         return p_center, avg_dir, v_sep
 
     def bound_pos(self, dim):
-        v = Vector2(0,0)
+        bound_multiplier = 2
+        v = Vector2(0, 0)
         x, y = self.pos.x, self.pos.y
         if x < 0:
             v.x = 10
@@ -53,7 +69,7 @@ class Boid():
             v.y = 10
         elif y > dim[1]:
             v.y = -10
-        return v
+        return v * bound_multiplier
 
     def goal(self, goal_packed):
         v_goal = Vector2(0, 0)
@@ -61,7 +77,7 @@ class Boid():
             goal_pos = goal_packed[0]
             attract = goal_packed[1]
             goal = Vector2(goal_pos[0], goal_pos[1])
-            if length(goal, self.pos) < self.sight_range:
+            if self.length(goal, self.pos) < self.sight_range:
                 attract_multiplier = 1 if attract else -1
                 v_goal += attract_multiplier * ((goal - self.pos) / 4)
         return v_goal
@@ -87,7 +103,7 @@ class Boid():
         #If Boid is given no velocity from any rules, it is given a random speed. Only happens before they form flocks
         if linear_velocity == 0:
             self.velocity = Vector2(random.uniform(-1,1), random.uniform(-1, 1)) * self.max_velocity
-        if self.is_max_velocity(self.velocity):
+        elif self.is_max_velocity(self.velocity):
             self.velocity = (self.velocity/linear_velocity) * self.max_velocity
 
         #This is only to get a point in front of boids position to draw the 'beak'
@@ -105,17 +121,80 @@ class Boid():
 class FlockController():
     def __init__(self, width, height, amt):
         self.width, self.height = width, height  # max width height of environment
+        self.swarm_grid = SwarmGrid(width, height)
         self.flock = self.initialize_boids(amt)
+        self.swarm_grid.populate(self.flock)
 
     def initialize_boids(self, amt):
         flock = []
         for i in range(0, amt):
-            flock.append(Boid(random.randint(0, self.width), random.randint(0, self.height)))
+            b = Boid(random.randint(0, self.width), random.randint(0, self.height))
+            flock.append(b)
         return flock
 
     def move_flock(self, goal, dt):
         for b in self.flock:
-            b.update(self.flock, goal, (self.width, self.height), dt)
+            local_swarm = self.swarm_grid.get_neighbourhood(b)
+            b.update(local_swarm, goal, (self.width, self.height), dt)
 
     def update(self, goal, dt):
         self.move_flock(goal, dt)
+        self.swarm_grid.update_grid()
+
+
+class SwarmGrid():
+    cell_size = 100
+    boids = []
+    def __init__(self, width, height):
+        x_cells = width // self.cell_size
+        y_cells = height // self.cell_size
+        self.x_max_width = int((x_cells - 1) * 1.2)
+        self.x_min_width = int(0 -(self.x_max_width * 0.2))
+        self.y_max_width = int((y_cells - 1) * 1.2)
+        self.y_min_width = int(0 -(self.y_max_width * 0.2))
+        self.cell_map = {}
+        self.search_range = self.cell_size
+        for x in range(self.x_min_width, self.x_max_width+1):
+            for y in range(self.y_min_width, self.y_max_width+1):
+                self.cell_map[(x,y)] = []
+
+    def populate(self, flock):
+        [self.add(b) for b in flock]
+
+    def cell_id(self,x_pos, y_pos):
+        x, y = x_pos//self.search_range, y_pos//self.search_range
+        #Store negative values in 0 cell
+        x = max(self.x_min_width, x)
+        x = min(x, self.x_max_width)
+        y = max(self.y_min_width, y)
+        y = min(y, self.y_max_width)
+        print x,y
+        return x,y
+
+    def add(self, b):
+        self.boids.append(b)
+        self._put_in_grid(b)
+
+    def _put_in_grid(self, b):
+        self.cell_map[self.cell_id(b.pos.x, b.pos.y)].append(b)
+
+    def get_neighbourhood(self, b):
+        neighbourhood = []
+        x, y = self.cell_id(b.pos.x, b.pos.y)
+        for i in range(-1,2):
+            for j in range(-1, 2):
+                new_x = x + i
+                new_y = y + j
+                new_x = max(new_x, 0)
+                new_x = min(new_x, self.x_max_width)
+                new_y = max(new_y, 0)
+                new_y = min(new_y, self.y_max_width)
+                neighbourhood += self.cell_map[(new_x, new_y)]
+        return neighbourhood
+
+    def update_grid(self):
+        for c in self.cell_map.values():
+            c[:] = []
+        [self._put_in_grid(b) for b in self.boids]
+
+
--- run.py
+++ run.py
@@ -7,46 +7,51 @@ from boid import FlockController
 def draw(flock):
     SCREEN.fill(BACKGROUND)
     for b in flock:
-        pygame.draw.circle(SCREEN, b.colour, (int(b.pos.x), int(b.pos.y)), 15, 0)
+        pygame.draw.circle(SCREEN, b.colour, (int(b.pos.x), int(b.pos.y)), 10, 0)
         pygame.draw.line(SCREEN, (200, 200, 200), (int(b.pos.x), int(b.pos.y)),
                          (int(b.pos.x + b.look_direction.x),   int(b.pos.y + b.look_direction.y)), 2)
 
 pygame.init()
 
-BOID_AMOUNT = 40
+BOID_AMOUNT = 100
 BACKGROUND = (69, 52, 218)
 WIDTH, HEIGHT = 1600, 1000
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-run = True
-clock = pygame.time.Clock()
-frames_rendered = 0
 
-mouse_active = False
-goal_attractive = True
-flock_controller = FlockController(WIDTH, HEIGHT, BOID_AMOUNT)
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    frames_rendered = 0
 
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pressed = pygame.mouse.get_pressed()
-            print pressed
-            if pressed[0]:
-                goal_attractive = not goal_attractive
-            elif pressed[2]:
-                mouse_active = not mouse_active
+    mouse_active = False
+    goal_attractive = True
+    flock_controller = FlockController(WIDTH, HEIGHT, BOID_AMOUNT)
 
-    pygame.event.pump()
-    if mouse_active:
-        mouse_pos = pygame.mouse.get_pos()
-    else:
-        mouse_pos = None
-    flock_controller.update((mouse_pos, goal_attractive), clock.get_time() / 1000.0)
-    draw(flock_controller.flock)
-    pygame.display.flip()
-    frames_rendered += 1
+    while run:
+        clock.tick(50)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pressed = pygame.mouse.get_pressed()
+                print pressed
+                if pressed[0]:
+                    goal_attractive = not goal_attractive
+                elif pressed[2]:
+                    mouse_active = not mouse_active
 
-print "FPS: " +str(frames_rendered/(pygame.time.get_ticks()/1000.0))
+        pygame.event.pump()
+        if mouse_active:
+            mouse_pos = pygame.mouse.get_pos()
+        else:
+            mouse_pos = None
+        flock_controller.update((mouse_pos, goal_attractive), clock.get_time() / 1000.0)
+        draw(flock_controller.flock)
+        pygame.display.flip()
+        frames_rendered += 1
+    #End Main Loop
+    print "FPS: " +str(frames_rendered/(pygame.time.get_ticks()/1000.0))
+
+main()
+
